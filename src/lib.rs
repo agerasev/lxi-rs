4544a763417b618db879a75ee3a6c0b1d9df1cdb
Alexey Gerasev <alexey.gerasev@gmail.com>
2019-02-12 19:16:50 +0700

    lxi device struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,25 @@
 #[cfg(test)]
+mod dummy;
+
+use std::io;
+use std::net::{TcpStream, SocketAddr};
+
+pub struct LxiDevice {
+    addr: SocketAddr,
+    stream: TcpStream,
+}
+
+impl LxiDevice {
+    pub fn connect(addr: SocketAddr) -> io::Result<LxiDevice> {
+        let stream = TcpStream::connect(addr)?;
+        Ok(LxiDevice {
+            addr: stream.peer_addr().unwrap(),
+            stream,
+        })
+    }
+}
+
+#[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
